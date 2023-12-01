40a93f4632fb0f435ae644df6cc4dc3932468dd7
strongreasons <abenkenari@gmail.com>
2023-12-01 21:22:34 +0000

    X00T: Rebrand to X00T
    Change-Id: I98d86b116160995a37cacaf8e089611345ec90c1

---
+++ rr_X00T.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common RR. stuff
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+TARGET_FACE_UNLOCK_SUPPORTED := true
+RR_BUILDTYPE := Oppenheimer
+BUILD_RR_WALLPAPERS := true
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Inherit from X00T device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := rr_X00T
+PRODUCT_DEVICE := X00T
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := Zenfone Max Pro M1
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=ASUS_X00T \
+    PRODUCT_NAME=WW_X00T \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+
+# Build fingerprint
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
