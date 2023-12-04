70236bdbf921af1da0d846a4ac0091e07145eada
dotkit <ewprjkt@proton.me>
2023-12-04 00:13:32 +0000

    X00T: Added and fixed some stuff
    Change-Id: I64406ee88a046e5bd16816c618c1c9a9e83bd70a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,7 +180,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 BOARD_VNDK_VERSION := current
