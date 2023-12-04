213d1852fd7cdf3c78defc1268a1251952c73599
dotkit <ewprjkt@proton.me>
2023-12-04 00:13:32 +0000

    X00T: Added and fixed some stuff
    Change-Id: I64406ee88a046e5bd16816c618c1c9a9e83bd70a

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,7 @@
+export SELINUX_IGNORE_NEVERALLOWS=true
+
 #rm -rf kernel/asus/sdm660
 git clone --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b codelinaro-hmp kernel/asus/sdm660
 
 rm -rf hardware/qcom-caf/msm8998/audio
-git clone https://github.com/strongreasons/hardware_qcom-caf_msm8998_audio -b ten hardware/qcom-caf/msm8998/audio
+git clone https://github.com/lineageX00T/hardware_qcom-caf_msm8998_audio -b lineage-17.1 hardware/qcom-caf/msm8998/audio
