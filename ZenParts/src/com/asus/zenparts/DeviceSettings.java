4789e239ec6dc8c5b4069cc6d26ae7b7dbed468e
Kneba <abenkenary3@gmail.com>
2023-12-10 06:46:51 +0000

    Revert "X00T: ZenParts: Use % for notification LED brightness"
    This reverts commit eb93e4bc64cd91cc65c22cc7acdff89d58f75eb1.
    
    Change-Id: I9ff3c47e3b90296cac8c7f43726c61df5a10a422

--- ZenParts/src/com/asus/zenparts/DeviceSettings.java
+++ ZenParts/src/com/asus/zenparts/DeviceSettings.java
@@ -95,9 +95,6 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final String NOTIF_LED_RED_PATH = "/sys/class/leds/red/max_brightness";
     public static final String NOTIF_LED_GREEN_PATH = "/sys/class/leds/green/max_brightness";
 
-    public static final int MIN_LED = 1;
-    public static final int MAX_LED = 255;
-
     private static final String SELINUX_CATEGORY = "selinux";
     private static final String PREF_SELINUX_MODE = "selinux_mode";
     private static final String PREF_SELINUX_PERSISTENCE = "selinux_persistence";
@@ -346,9 +343,9 @@ public class DeviceSettings extends PreferenceFragment implements
                 break;
 
             case PREF_NOTIF_LED:
-                FileUtils.setValue(NOTIF_LED_BLUE_PATH, (int) value / 100.0 * (MAX_LED - MIN_LED) + MIN_LED);
-                FileUtils.setValue(NOTIF_LED_RED_PATH, (int) value / 100.0 * (MAX_LED - MIN_LED) + MIN_LED);
-                FileUtils.setValue(NOTIF_LED_GREEN_PATH, (int) value / 100.0 * (MAX_LED - MIN_LED) + MIN_LED);
+                FileUtils.setValue(NOTIF_LED_BLUE_PATH, (int) value);
+                FileUtils.setValue(NOTIF_LED_RED_PATH, (int) value);
+                FileUtils.setValue(NOTIF_LED_GREEN_PATH, (int) value);
                 break;
 
             case PREF_SELINUX_MODE:
