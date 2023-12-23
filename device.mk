bf6ee4f163d6aea1c072b27cdb0eac5c7945268e
dotkit <ewprjkt@proton.me>
2023-12-23 16:18:04 +0000

    X00T: Build android.hardware.graphics.allocator
    Change-Id: Iff17ec5b5330cb313cd70ce16e58ad25ec98c9a1

--- device.mk
+++ device.mk
@@ -156,6 +156,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    android.hardware.graphics.allocator@3.0-impl \
     android.frameworks.displayservice@1.0
 
 # Doze mode
