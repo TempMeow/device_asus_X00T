9a247473a3507cebdb05d84061ca5fa285524193
fakedotkit <ewprjkt@proton.me>
2023-12-24 18:04:53 +0700

    X00T: regenerate

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,5 +3,5 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 #rm -rf kernel/asus/sdm660
 git clone --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b codelinaro-hmp kernel/asus/sdm660
 
-rm -rf hardware/qcom-caf/msm8998/audio
-git clone https://github.com/lineageX00T/hardware_qcom-caf_msm8998_audio -b lineage-17.1 hardware/qcom-caf/msm8998/audio
+#rm -rf hardware/qcom-caf/msm8998/audio
+#git clone https://github.com/lineageX00T/hardware_qcom-caf_msm8998_audio -b lineage-17.1 hardware/qcom-caf/msm8998/audio
