6c3c0315d8296fefe1d0401c50243432dd00a460
Meisie Widuri <ewprjkt@proton.me>
2024-08-04 02:38:18 +0700

    X00T: Change kerneltree

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,9 @@
-git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 kernel/asus/sdm660
+rm -rf kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b tom/eol/hmp kernel/asus/sdm660
 rm -rf kernel/asus/sdm660/KernelSU/userspace
+rm -rf kernel/asus/sdm660/KernelSU/website
+rm -rf kernel/asus/sdm660/KernelSU/scripts
+rm -rf kernel/asus/sdm660/KernelSU/docs
 
 rm -rf hardware/qcom-caf/msm8998/audio
 git clone --depth=1 https://github.com/inexdroid/hardware_qcom-caf_msm8998_audio hardware/qcom-caf/msm8998/audio
