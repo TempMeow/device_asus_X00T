b08fa9de6af796a24a1937a456eed6fc92befbf4
Kneba <abenkenary3@gmail.com>
2024-10-30 23:14:19 +0700

    X00T: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,5 @@
 rm -rf kernel/asus/sdm660
-git clone --depth=1 --recursive https://github.com/texascake/kernel_asus_sdm660 --single-branch kernel/asus/sdm660
-rm -rf kernel/asus/sdm660/KernelSU/userspace
-rm -rf kernel/asus/sdm660/KernelSU/website
-rm -rf kernel/asus/sdm660/KernelSU/scripts
-rm -rf kernel/asus/sdm660/KernelSU/docs
+git clone --depth=1 https://github.com/Tiktodz/android_kernel_asus_sdm660 -b 4.4 kernel/asus/sdm660
 
 rm -rf device/asus/X00T
 git clone --depth=1 https://github.com/Tiktodz/device_asus_X00T device/asus/X00T
@@ -17,7 +13,8 @@ git clone --depth=1 https://github.com/inexdroid/hardware_qcom-caf_msm8998_displ
 rm -rf hardware/qcom-caf/msm8998/media
 git clone --depth=1 https://github.com/inexdroid/hardware_qcom-caf_msm8998_media hardware/qcom-caf/msm8998/media
 
-git clone --depth=1 https://github.com/Tiktodz/vendor -b everest kntl && cp -R kntl/* vendor/ && rm -rf kntl
+rm -rf vendor/lineage-priv/keys
+git clone https://github.com/sotodrom/keys vendor/lineage-priv/keys
 
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/Tiktodz/vendor_asus vendor/asus
